a4d61ee22f3caca628f90ec7c50526405242b264
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-04-28 20:25:47 +0200

    functions for calling my api

--- src/ts/rest.ts
+++ src/ts/rest.ts
@@ -1,4 +1,33 @@
-export const fetchUsers = async (userId: number): Promise<Record<string, string>> => {
-    const res = await fetch(`http://localhost:8081/api/users/${userId}`)
-    return res.json()
+interface User {
+    userID: number,
+    name: string,
+    birthdate: Date,
+    role: string,
+}
+
+interface Post {
+    postID: number,
+    content: string,
+    postedAt: Date,
+    userId: number,
+    username: string,
+    upVotes: number,
+}
+
+export const fetchUsers = async (): Promise<User[]> => {
+    const res = await fetch('http://localhost:8081/api/users')
+    const data = await res.json()
+    return data.map((user: User) => ({
+        ...user,
+        birthdate: new Date(user.birthdate)
+    }))
+}
+
+export const fetchPosts = async (): Promise<Post[]> => {
+    const res = await fetch('http://localhost:8081/api/posts')
+    const data = await res.json()
+    return data.map((post: Post) => ({
+        ...post,
+        postedAt: new Date(post.postedAt)
+    }))
 }
